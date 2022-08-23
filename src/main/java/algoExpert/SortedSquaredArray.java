5172afa6811fc75daea25b6240bbce0f4f26f53a
Vusala <vusalahasanova423@gmail.com>
2022-08-23 15:57:15 +0300

    sorted squared array

--- src/main/java/algoExpert/SortedSquaredArray.java
+++ src/main/java/algoExpert/SortedSquaredArray.java
@@ -10,6 +10,17 @@ public class SortedSquaredArray {
             answer[i] = array[i]* array[i];
         }
         Arrays.sort(answer);
+        for(int j = 0; j< array.length; j++){
+            for (int k = j+1; k < array.length ; k++) {
+                int temp = 0;
+                if(answer[j]>answer[k]){
+                    temp = answer[j];
+                    answer[j]=answer[k];
+                    answer[k]= temp;
+
+                }
+            }
+        }
         return answer;
     }
 }
