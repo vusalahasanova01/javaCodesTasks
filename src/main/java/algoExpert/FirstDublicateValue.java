e9fc8b861deec3715a1adf571e9285f0b9108a5c
vusalahasanova01 <vusalahasanova423@gmail.com>
2022-10-15 00:12:29 +0400

    find first dublicate value

---
+++ src/main/java/algoExpert/FirstDublicateValue.java
@@ -0,0 +1,32 @@
+package algoExpert;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class FirstDublicateValue {
+  public  int firstDuplicateValue(int[] array) {
+    if (array.length==0){ return -1;}
+    ArrayList<Integer> arrayList = new ArrayList<>();
+    for (int i = 0; i < array.length-1 ; i++) {
+      for (int j = 0; j <array.length; j++) {
+        if (array[i]==array[j]&& i!=j && i<j){
+          arrayList.add(j);
+        }
+      }
+    }
+    if(arrayList.size()==0){
+      return -1;
+    }
+    else {
+      Collections.sort(arrayList);
+      return array[arrayList.get(0)];
+    }
+  }
+
+  public static void main(String[] args) {
+    int [] arr = new int[]{2,1,5,2,3,3,4};
+    FirstDublicateValue firstDublicateValue = new FirstDublicateValue();
+    int answer = firstDublicateValue.firstDuplicateValue(arr);
+    System.out.println(answer);
+  }
+}
